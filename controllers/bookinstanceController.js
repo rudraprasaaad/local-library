00b1d6da5eb69cf09708663f85d8ce17a47c00ba
rudraprasaaad <goudarudra23@gmail.com>
2024-10-30 07:13:10 +0530

    modified genre and bookinstance controller

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,4 @@
-const BookInstance = require("../models/bookinstance");
+const BookInstance = require("../models/bookinstance.js");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all BookInstances.
